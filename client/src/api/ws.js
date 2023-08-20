2696d45edbcd56ff66d92279cfbaecd144ae82ea
Viktoriia <pecourses>
2023-08-20 11:03:58 +0300

    Add reducers for creating mes

--- client/src/api/ws.js
+++ client/src/api/ws.js
@@ -1,16 +1,22 @@
 import { io } from 'socket.io-client';
+import {
+  createMessageError,
+  createMessageFullfilled,
+} from '../store/slices/messagesSlice';
 
 const socket = io('ws://localhost:5000'); // => 'connection' on server
 
 export const createMessageWs = payload => socket.emit('NEW_MESSAGE', payload);
 
-socket.on('NEW_MESSAGE_CREATED', payload => {
-  console.log('payload :>> ', payload);
-});
+export function bringStoreToWs(store) {
+  socket.on('NEW_MESSAGE_CREATED', payload => {
+    store.dispatch(createMessageFullfilled(payload));
+  });
 
-socket.on('NEW_MESSAGE_ERROR', payload => {
-  console.log('error :>> ', payload);
-});
+  socket.on('NEW_MESSAGE_ERROR', payload => {
+    store.dispatch(createMessageError(payload));
+  });
+}
 
 export default socket;
 
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import { bringStoreToWs } from '../api/ws';
 
 const store = configureStore({ reducer: rootReducer });
+
+bringStoreToWs(store);
 
 export default store;
